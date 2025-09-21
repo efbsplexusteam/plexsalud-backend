d5fc1e8a5638934c8b0d50faee059f1a9406ee3c
mavcr <mianvacrro@gmail.com>
2025-09-21 21:57:11 +0000

    patient create and get

--- backend/src/main/java/com/plexsalud/plexsalud/patient/controllers/PatientController.java
+++ backend/src/main/java/com/plexsalud/plexsalud/patient/controllers/PatientController.java
@@ -1,39 +1,70 @@
 package com.plexsalud.plexsalud.patient.controllers;
 
-import java.net.URI;
-import java.util.List;
+import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import com.plexsalud.plexsalud.user.entities.User;
-import com.plexsalud.plexsalud.user.services.UserService;
+import com.plexsalud.plexsalud.auth.services.JwtService;
+import com.plexsalud.plexsalud.patient.dtos.PatientDto;
+import com.plexsalud.plexsalud.patient.responses.PatientResponse;
+import com.plexsalud.plexsalud.patient.services.PatientService;
+import com.plexsalud.plexsalud.user.entities.Role;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 
-@Tag(name = "Enfermeros", description = "Operaciones con enfermeros")
+@Tag(name = "Patients", description = "Operations with patients")
 @SecurityRequirement(name = "bearerAuth")
 @RequestMapping("/api/v1/patients")
 @RestController
 public class PatientController {
-    @Autowired
-    private final UserService userService;
 
-    public PatientController(UserService userService) {
-        this.userService = userService;
+    private final PatientService patientService;
+    private final JwtService jwtService;
+
+    public PatientController(PatientService patientService, JwtService jwtService) {
+        this.patientService = patientService;
+        this.jwtService = jwtService;
     }
 
-   
+    @PostMapping
+    public ResponseEntity<PatientResponse> savePatient(HttpServletRequest request,
+            @RequestBody PatientDto patientDto) {
+        UUID uuid = jwtService.extractUuid(request);
+        Role role = jwtService.extractRole(request);
+
+        if (role != Role.PATIENT) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Role Invalid");
+        }
+
+        patientDto.setUserUuid(uuid);
+        PatientResponse saved = patientService.save(patientDto);
+        return ResponseEntity.ok(saved);
+    }
+
+    @GetMapping("self")
+    public ResponseEntity<PatientResponse> findSelf(HttpServletRequest request) {
+        UUID uuid = jwtService.extractUuid(request);
+
+        PatientResponse patientResponse = patientService.findOneByUser(uuid);
+
+        return ResponseEntity.ok(patientResponse);
+    }
+
+    @GetMapping("uuid")
+    public ResponseEntity<PatientResponse> findOne(@RequestParam("uuid") UUID uuid) {
+        PatientResponse patientResponse = patientService.findOne(uuid);
+
+        return ResponseEntity.ok(patientResponse);
+    }
 
 }
