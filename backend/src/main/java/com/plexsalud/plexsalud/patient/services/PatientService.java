9d6593640dc8ad36d5c21db4f70c88dca0b7d2f7
mavcr <mianvacrro@gmail.com>
2025-09-21 21:57:11 +0000

    patient create and get

--- backend/src/main/java/com/plexsalud/plexsalud/patient/services/PatientService.java
+++ backend/src/main/java/com/plexsalud/plexsalud/patient/services/PatientService.java
@@ -1,38 +1,56 @@
 package com.plexsalud.plexsalud.patient.services;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.plexsalud.plexsalud.patient.dtos.PatientDto;
+import com.plexsalud.plexsalud.patient.responses.PatientResponse;
+import com.plexsalud.plexsalud.patient.entities.Patient;
+import com.plexsalud.plexsalud.patient.repositories.PatientRepository;
 import com.plexsalud.plexsalud.user.entities.User;
 import com.plexsalud.plexsalud.user.repositories.UserRepository;
-
 
 @Service
 public class PatientService {
     private final UserRepository userRepository;
+    private final PatientRepository patientRepository;
 
-    public PatientService(UserRepository userRepository) {
+    public PatientService(PatientRepository patientRepository, UserRepository userRepository) {
+        this.patientRepository = patientRepository;
         this.userRepository = userRepository;
     }
 
-    public List<User> getAllUsers() {
-        List<User> users = new ArrayList<>();
+    public PatientResponse save(PatientDto patientDto) {
+        Patient patient = new Patient();
 
-        userRepository.findAll().forEach(users::add);
+        User user = userRepository.findById(patientDto.getUserUuid())
+                .orElseThrow(() -> new RuntimeException("Patient not found with uuid: " + patientDto.getUserUuid()));
 
-        return users;
+        patient.setFullName(patientDto.getFullName());
+        patient.setUser(user);
+
+        patient = patientRepository.save(patient);
+        PatientResponse PatientResponse = new PatientResponse(patient.getFullName());
+
+        return PatientResponse;
     }
 
-    public User getUser(UUID uuid) {
-        Optional<User> userOptional = userRepository.findById(uuid);
-        if (userOptional.isPresent()) {
-            return userOptional.get();
-        } else {
-            throw new RuntimeException("User not found with UUID: " + uuid);
-        }
+    public PatientResponse findOne(UUID uuid) {
+        Patient patient = patientRepository.findById(uuid)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Patient not found with uuid: " + uuid));
+
+        return new PatientResponse(patient.getFullName());
+    }
+
+    public PatientResponse findOneByUser(UUID uuid) {
+        Patient patient = patientRepository.findByUser(new User().setUuid(uuid))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Patient not found with uuid: " + uuid));
+
+        return new PatientResponse(patient.getFullName());
     }
 }
