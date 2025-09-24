9693314860208bb0e8d2e1fb4666fe88e0374f07
mavcr <mianvacrro@gmail.com>
2025-09-24 12:07:24 +0000

    Closes #18

--- backend/src/main/java/com/plexsalud/plexsalud/appointment/controllers/AppointmentController.java
+++ backend/src/main/java/com/plexsalud/plexsalud/appointment/controllers/AppointmentController.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -85,5 +88,16 @@ public class AppointmentController {
         appointmentDto.setPatientUuid(uuid);
 
         return appointmentService.createAppointment(appointmentDto);
+    }
+
+    @DeleteMapping("/{uuid}")
+    public ResponseEntity<String> deleteAppointment(@PathVariable("uuid") UUID uuid) {
+        boolean isDeleted = appointmentService.deleteAppointment(uuid);
+        
+        if (isDeleted) {
+            return ResponseEntity.ok("Appointment deleted successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Appointment not found");
+        }
     }
 }
